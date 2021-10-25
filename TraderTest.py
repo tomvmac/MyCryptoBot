7cecdec1d5916aad4de489df24213151c935cf02
mctm <tomvmac@yahoo.com>
2021-10-25 14:30:07 -0400

    Added logic for open and closed trades.

--- TraderTest.py
+++ TraderTest.py
@@ -1,8 +1,10 @@
 import Trader
 
-trade1 = {"symbol": "ETHUSD", "type": "BUY", "status": "OPEN", "price": 4000, "qty": 10}
-trade2 = {"symbol": "CRVUSD", "type": "BUY", "status": "OPEN", "price": 150, "qty": 10}
+trade1 = {"symbol": "ETHUSD", "type": "BUY", "tradeDate": "", "tradeTime": "", "status": "OPEN", "price": 4000, "qty": 10}
+trade2 = {"symbol": "CRVUSD", "type": "BUY", "tradeDate": "", "tradeTime": "", "status": "OPEN", "price": 150, "qty": 10}
 
 
 print(Trader.openTrade(trade1))
 print(Trader.openTrade(trade2))
+
+
