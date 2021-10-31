a410fdeb3e15a0a41a075b3bc3403bdfec9db7b5
mctm <tomvmac@yahoo.com>
2021-10-31 17:31:04 -0400

    Updated BinanceClient with getHistoricalPrices function.

--- core/api/binance/BinanceClient.py
+++ core/api/binance/BinanceClient.py
@@ -18,6 +18,17 @@ def getPrice(symbol):
     # get balance for a specific asset only (BTC)
     return client.get_symbol_ticker(symbol=symbol)
 
+def getHistoricalPrices(symbol, interval, startTimestamp):
+    # valid intervals - 1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M
+
+    # get timestamp of earliest date data is available
+    #timestamp = client._get_earliest_valid_timestamp(symbol, interval)
+
+    # request historical candle (or klines) data
+    bars = client.get_historical_klines(symbol, interval, startTimestamp, limit=100)
+
+    return bars
+
 def getPrices():
     # get latest price from Binance API
     prices = client.get_symbol_ticker()
--- test/BinanceClientTest.py
+++ test/BinanceClientTest.py
@@ -1,4 +1,6 @@
 from core.api.binance import BinanceClient
+from datetime import datetime, timedelta
+
 
 # Get Account Info
 print(BinanceClient.getAccountInfo())
@@ -27,3 +29,37 @@ print("----------------")
 
 # Sell Market Order
 # print(BinanceClient.sellMarketOrder("USDTUSD", 50))
+
+# Get historical prices
+yesterday = datetime.today() - timedelta(days = 1)
+yesterdayStr = str(yesterday)
+nowStr = str(datetime.now())
+historicalPriceBars = BinanceClient.getHistoricalPrices("BTCUSD", "4h", yesterdayStr)
+print("-----------------------")
+print("historical prices:")
+print(historicalPriceBars)
+
+# latest closing price
+firstBarIndex = 0
+firstBar = historicalPriceBars[0]
+firstBarClosePrice = historicalPriceBars[0][4]
+# print(firstBar)
+print("First Bar Close Price:", firstBarClosePrice)
+
+midBarIndex = int(len(historicalPriceBars) / 2)
+# print("Mid Bar Index" , midBarIndex)
+midBar = historicalPriceBars[midBarIndex]
+midBarClosePrice = historicalPriceBars[midBarIndex][4]
+print("Mid Bar Close Price", midBarClosePrice)
+
+lastBarIndex = len(historicalPriceBars)
+lastBar = historicalPriceBars[lastBarIndex-1]
+lastBarClosePrice = historicalPriceBars[lastBarIndex-1][4]
+print("Last Bar Close Price", lastBarClosePrice)
+
+# get date from timestamp
+# timestampStart = 1545730073
+# timestampEnd = 1545730073
+#
+# print(datetime.fromtimestamp(timestampStart))
+# print(datetime.fromtimestamp(timestampEnd))
